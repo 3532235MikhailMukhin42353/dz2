362ff7e4bf81db0252a3780aff2943ab9f0f3a0a
3532235MikhailMukhin42353 <dzhon.seryy@mail.ru>
2022-12-02 22:17:57 +0300

    add Z_19 sg gs :wq

--- Z_19/Program.cs
+++ Z_19/Program.cs
@@ -1,4 +1,11 @@
 ﻿// 19. Принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
-int [] array = {1,2,3,2,1};
-if(array[0] == array[4] && array[1] == array[3]) Console.WriteLine("Число палиндром");
-else Console.WriteLine("Число не палиндром");
+try
+{
+    int[] array = { 1, 2, 3, 2, 1 };
+    if (array[0] == array[4] && array[1] == array[3]) Console.WriteLine("Число палиндром");
+    else Console.WriteLine("Число не палиндром");
+}
+catch
+{
+    Console.WriteLine("Введите именно цифры!")
+}
