88f81384913c39135c1d165b627dec0f3c099fc9
3532235MikhailMukhin42353 <dzhon.seryy@mail.ru>
2022-12-03 17:23:39 +0300

    add Z_23fsfsf :wq

---
+++ Z_23/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.   5=5^3 
+try
+{
+    Console.WriteLine("Введите число:");
+    int N = Convert.ToInt32(Console.ReadLine());
+    for (int i = 0; i <= N; i++)
+    Console.WriteLine(i * i * i);
+}
+catch
+{
+    Console.WriteLine("Введите именно цифру!");
+}
